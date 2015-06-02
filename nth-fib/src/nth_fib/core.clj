7f906172547bc2fbb035adaca267d9cf84e391d2
Sujay Bothe <ssbothe3@gmail.com>
2015-06-02 22:04:50 +0530

    commit

--- nth-fib/src/nth_fib/core.clj
+++ nth-fib/src/nth_fib/core.clj
@@ -2,20 +2,19 @@
   (:gen-class))
 
 (defn nth-fib [n]
-	(loop [n1 0 n2 1 i 2]
-		(if (<= i n)
-			(recur n2 (+ n1 n2) (inc i))
-			(do
-				(println "nth fib number:")
-				(println n2)
-			)
-		)	
-	)
+  (loop [n1 0 n2 1 i 2]
+    (if (<= i n)
+      (recur n2 (+ n1 n2) (inc i))
+      (do (println "nth fib number:")
+        (println n2)
+      )
+    )	
+  )
 )
 
 (defn -main [& args]
-	(println "Enter n")
- 	(let [n (read-string (read-line))]
-  		(nth-fib n)
-  	) 
+  (println "Enter n")
+  (let [n (read-string (read-line))]
+    (nth-fib n)
+  ) 
 )  
--- proc-sum/src/proc_sum/core.clj
+++ proc-sum/src/proc_sum/core.clj
@@ -10,22 +10,28 @@
 (defn tt [n a]
 	(+ (- (+ (- (* 2 a a)  (* 2 a)) (* 2 a n)) n) (* n n)))
 
+(defn sum [start_int end_int f]
+  (if (= f 0)
+    (/ (* (+ (- end_int start_int) 1) (+ start_int end_int)) 2)
+    (let [n (+ (- end_int start_int) 1)]	
+      (* (ft n) (st n start_int) (tt n start_int))
+    )
+  )    
+)
+
 (defn procsum [start_int end_int] 
-	
-	(def sum (/ (* (+ (- end_int start_int) 1) (+ start_int end_int)) 2)) ;(n(a1+a2)/2)
-	(println "sum:")
-	(println sum) 
-	
-	(def a start_int)
-	(def n (+ (- end_int start_int) 1))
-	(def sum (* (ft n) (st n a) (tt n a)))
-	(println "sum of cubes:")
-	(println sum) 
+  (let [sum1 (sum start_int end_int 0) 
+        sum2 (sum start_int end_int 1)]   ;(n(a1+a2)/2)
+    (printf "sum:%d" sum1)
+    (printf "\nsum of cubes")
+    (println sum2)
+  ) 
 )
 
 (defn -main [& args]
-	(println "Enter start integer and end integer")	
-	(let [start_int (read-string (read-line)) end_int (read-string (read-line))]
-  		(procsum start_int end_int)
+  (println "Enter start integer and end integer")	
+  (let [start_int (read-string (read-line)) 
+	end_int (read-string (read-line))]
+    (procsum start_int end_int)
   )
 )
